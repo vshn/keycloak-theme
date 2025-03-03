2b727360fe99d04f99c93763c3bd6b6db25c180f
Stefanie Erne <stefanie.erne@nxt.engineering>
2025-03-03 09:00:27 +0100

    🎨 remove whitespace, new line at end of file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-/opt/keycloak/bin/kc.sh import --file /opt/keycloak/data/import/master.json 
+/opt/keycloak/bin/kc.sh import --file /opt/keycloak/data/import/master.json
 /opt/keycloak/bin/kc.sh start-dev --debug --spi-theme-static-max-age=-1 --spi-theme-cache-themes=false --spi-theme-cache-templates=false
- 
